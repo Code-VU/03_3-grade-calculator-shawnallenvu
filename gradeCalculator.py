018a080bd11050223866b7fc88b2acd472a3fbe0
Shawn Allen <Shawn.Allen@vu.com>
2025-03-14 09:25:32 -0500

    Added try except for bad values. Prints grade for 0-1 float values

--- gradeCalculator.py
+++ gradeCalculator.py
@@ -1,9 +1,37 @@
 def calculateGrade():
-    # Implement your solution in between the two comment blocks
     print("Calculating Grade")
-    # This first line is provided for you
 
-    hrs = float(input("Enter score:"))
+    # Grading Criterion
+    #  Score   Grade
+    # >= 0.9     A
+    # >= 0.8     B
+    # >= 0.7     C
+    # >= 0.6     D
+    #  < 0.6     F
+
+    # Try except for bad user input. Takes score from user 
+    try:
+        score = float(input("Enter score (0.0 - 1.0) "))
+        if not 0.0 <= score <= 1.0: # Checks to make sure score is within bounds
+            raise ValueError # forces error so we don't have to write extra code 
+    except:
+        print("Bad score")
+        return
+    
+    # Match case instead of if else. Keeps comparisons to a minimum and keeps code clean
+    match score: 
+        case _ if score >= 0.9:
+            grade = 'A'
+        case _ if score >= 0.8:
+            grade = 'B'
+        case _ if score >= 0.7:
+            grade = 'C'
+        case _ if score >= 0.6:
+            grade = 'D'
+        case _: # Default to F if it doesn't meet above scores
+            grade = 'F'
+
+    print(grade) # Print grade
 
     # end assignment
 
